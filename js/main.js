94930638b47afa994572dc334ea0a97e186e9d13
Bastien Bourdon <bb@edgefolio.com>
2017-04-02 16:22:25 +0100

    The landing page is the posts list with the cover collapsed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 layout: null
 ---
 $(document).ready(function () {
-  $('a.blog-button').click(function (e) {
+  $('a.about-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
       $('.panel-cover').css('width', '100%')
       $('.panel-cover').animate({'max-width': '1600px'}, 400, swing = 'swing', function () {})
@@ -20,20 +20,12 @@ $(document).ready(function () {
     }
   })
 
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
-  $('.navigation-wrapper .blog-button').click(function () {
+  $('.navigation-wrapper .about-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
