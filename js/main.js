349b1fe72d1e0eb49593eb5144f274c12d81463c
Bastien Bourdon <bb@edgefolio.com>
2017-04-05 21:45:27 +0100

    Make the ABout button link to the about page on mobile and collapse the cover on desktop

--- js/main.js
+++ js/main.js
@@ -3,20 +3,19 @@ layout: null
 ---
 $(document).ready(function () {
   $('a.about-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
-      $('.panel-cover').css('width', '100%')
-      $('.panel-cover').animate({'max-width': '1600px'}, 400, swing = 'swing', function () {})
-      $('.panel-cover').removeClass('panel-cover--collapsed')
+    currentWidth = $('body').width()
+    if (currentWidth < 960) {
+      window.location = '/about/';
     } else {
-      currentWidth = $('.panel-cover').width()
-      if (currentWidth < 960) {
-        $('.panel-cover').addClass('panel-cover--collapsed')
-        $('.content-wrapper').addClass('animated slideInRight')
+      if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
+        $('.panel-cover').css('width', '100%')
+        $('.panel-cover').animate({'max-width': '1600px'}, 400, swing = 'swing', function () {})
+        $('.panel-cover').removeClass('panel-cover--collapsed')
       } else {
-        $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-        $('.panel-cover').addClass('panel-cover--collapsed')
-      }
+          $('.panel-cover').css('max-width', currentWidth)
+          $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+          $('.panel-cover').addClass('panel-cover--collapsed')
+        }
     }
   })
 
